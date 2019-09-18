5ac61e782dc886fa468059613231ae27a0bc671d
fengtian9311 <44286302+fengtian9311@users.noreply.github.com>
2019-09-18 14:47:08 +0800

    Update B1030.cpp

--- B1030.cpp
+++ B1030.cpp
@@ -7,12 +7,8 @@ int main(){
 	int data[N];
 	for(int i=0;i<N;i++) scanf("%d",&data[i]);
 	sort(&data[0],&data[N]);
-	int min=0,max=0,cnt=0;
-	while(p>=data[max]/(double)data[min]&&max<N){
-		max++;
-		cnt++;
-	}
-	for(int i=1;i+cnt-1<N;i++){
+	int cnt=0;
+	for(int i=0;i+cnt-1<N;i++){
 		for(int j=i+cnt-1;j<N-1;j++){
 			if(p>=data[j]/(double)data[i]&&p>=data[j+1]/(double)data[i]) cnt++;
 			else break;
